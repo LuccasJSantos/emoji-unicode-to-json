631809173dd042ffad108d1dd5c8d15248752a6d
Luccas Santos <luccasjsantos@hotmail.com>
2021-05-16 04:05:58 -0300

    :recycle: Refactor some lines here and there

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const fs = require('fs')
 const { includes, match, pipe, head, map, cond, T, always, filter, isNil, complement, split, converge, objOf, tail, mergeAll } = require('ramda')
 const str = fs.readFileSync('./emoji.txt', 'utf-8')
 
-const groupMatch = match(/(?=# group).+?(?=(?:# group|# Flags subtotal:))/gs)
 const isGroup = includes('group:')
 const isEmoji = includes('fully-qualified     # ')
 const getGroup = pipe(match(/(?<=# group: ).+/g), head)
 const getEmoji = pipe(match(/(?<=fully-qualified     # ).+?(?= E)/g), head)
-const toEmojiMap = converge(objOf, [head, tail])
 
 const fetchEmojis = pipe(
-  groupMatch,
+  match(/(?=# group).+?(?=(?:# group|# Flags subtotal:))/gs),
   map(pipe(
     split('\n'),
     map(cond([
@@ -19,14 +17,12 @@ const fetchEmojis = pipe(
       [T, always(undefined)]
     ])),
     filter(complement(isNil)),
-    toEmojiMap 
+    converge(objOf, [head, tail])
   )),
   mergeAll
 )
 
-const result = fetchEmojis(str)
-
-fs.writeFile('emojis.json', JSON.stringify(result), function (err) {
+fs.writeFile('emojis.json', JSON.stringify(fetchEmojis(str)), function (err) {
   if (err) return console.error('Error:', err)
 
   console.log('File created')
